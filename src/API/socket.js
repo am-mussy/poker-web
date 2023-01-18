6ce5472997e2eeee21e3245d37f0e6572d5b3124
am.mussy <aydoganwork@gmail.com>
2023-01-18 15:07:15 +0300

    port

--- src/API/socket.js
+++ src/API/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001/", {
+export const socket = io("localhost:3001", {
   transports: ["websocket"],
+  withCredentials: true,
 });
 
 export const JOIN_TO_ROOM = "JOIN_TO_ROOM";
