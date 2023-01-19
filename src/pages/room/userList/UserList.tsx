79619c5884e5ed4435069d6ee121109476f77861
am.mussy <aydoganwork@gmail.com>
2023-01-19 11:50:49 +0300

    fix linter

--- src/pages/room/userList/UserList.tsx
+++ src/pages/room/userList/UserList.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import {IUser} from "../../../types/types";
-import './userList.css'
+import React from "react";
+import { IUser } from "../../../types/types";
+import "./userList.css";
 import UserLine from "./userLine/UserLine";
 
 type UserListProps = {
-    users: IUser[]
-}
+  users: IUser[];
+};
 
-const UserList = ({users}:UserListProps) => {
-
-    return (
-        <div className={'user-list-root'}>
-            {users.map(({name,scram})=><UserLine key={`${name}_key`} name={name ?? 'NO NAME'} scram={scram}/>)}
-        </div>
-    );
-}
+const UserList = ({ users }: UserListProps) => {
+  return (
+    <div className={"user-list-root"}>
+      {users.map(({ name, scram }, index) => (
+        <UserLine key={`${index}_key`} name={name ?? "NO NAME"} scram={scram} />
+      ))}
+    </div>
+  );
+};
 
 export default UserList;
